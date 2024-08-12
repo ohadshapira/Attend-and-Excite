23f74e378cf03895ed3c6d4798b75330bebdc282
noaY8 <noayamin@mail.tau.ac.il>
2024-08-12 11:28:15 +0300

    Update pipeline_attend_and_excite3.py

--- pipeline_attend_and_excite3.py
+++ pipeline_attend_and_excite3.py
@@ -188,66 +188,11 @@ class AttendAndExcitePipeline(StableDiffusionPipeline):
 
         return text_inputs, prompt_embeds
 
-    def _compute_max_attention_per_index(self,
-                                         attention_maps: torch.Tensor,
-                                         indices_to_alter: List[int],
-                                         smooth_attentions: bool = False,
-                                         sigma: float = 0.5,
-                                         kernel_size: int = 3,
-                                         normalize_eot: bool = False) -> List[torch.Tensor]:
-        """ Computes the maximum attention value for each of the tokens we wish to alter. """
-        last_idx = -1
-        if normalize_eot:
-            prompt = self.prompt
-            if isinstance(self.prompt, list):
-                prompt = self.prompt[0]
-            last_idx = len(self.tokenizer(prompt)['input_ids']) - 1
-        attention_for_text = attention_maps[:, :, 1:last_idx]
-        attention_for_text *= 100
-        attention_for_text = torch.nn.functional.softmax(attention_for_text, dim=-1)
-
-        # Shift indices since we removed the first token
-        indices_to_alter = [index - 1 for index in indices_to_alter]
-
-        # Extract the maximum values
-        max_indices_list = []
-        for i in indices_to_alter:
-            image = attention_for_text[:, :, i]
-            if smooth_attentions:
-                smoothing = GaussianSmoothing(channels=1, kernel_size=kernel_size, sigma=sigma, dim=2).cuda()
-                input = F.pad(image.unsqueeze(0).unsqueeze(0), (1, 1, 1, 1), mode='reflect')
-                image = smoothing(input).squeeze(0).squeeze(0)
-            max_indices_list.append(image.max())
-        return max_indices_list
-
-    def _aggregate_and_get_max_attention_per_token(self, attention_store: AttentionStore,
-                                                   indices_to_alter: List[int],
-                                                   attention_res: int = 16,
-                                                   smooth_attentions: bool = False,
-                                                   sigma: float = 0.5,
-                                                   kernel_size: int = 3,
-                                                   normalize_eot: bool = False):
-        """ Aggregates the attention for each token and computes the max activation value for each token to alter. """
-        attention_maps = aggregate_attention(
-            attention_store=attention_store,
-            res=attention_res,
-            from_where=("up", "down", "mid"),
-            is_cross=True,
-            select=0)
-        max_attention_per_index = self._compute_max_attention_per_index(
-            attention_maps=attention_maps,
-            indices_to_alter=indices_to_alter,
-            smooth_attentions=smooth_attentions,
-            sigma=sigma,
-            kernel_size=kernel_size,
-            normalize_eot=normalize_eot)
-        return max_attention_per_index
-
     @staticmethod
-    def _compute_loss(max_attention_per_index: List[torch.Tensor], return_losses: bool = False) -> torch.Tensor:
-        """ Computes the attend-and-excite loss using the maximum attention value for each token. """
-        losses = [max(0, 1. - curr_max) for curr_max in max_attention_per_index]
-        loss = max(losses)
+    def _compute_loss(desired_count:List[int] , num_obj_per_index: List[torch.Tensor], return_losses: bool = False) -> torch.Tensor:
+        """ Computes the lookahead loss using the difference in number of objects value for each token. """
+        losses = [(desired_count - num_obj) ** 2 for num_obj in num_obj_per_index]
+        loss = sum(losses)
         if return_losses:
             return loss, losses
         else:
@@ -259,88 +204,6 @@ class AttendAndExcitePipeline(StableDiffusionPipeline):
         grad_cond = torch.autograd.grad(loss.requires_grad_(True), [latents], retain_graph=True)[0]
         latents = latents - step_size * grad_cond
         return latents
-
-    def _perform_iterative_refinement_step(self,
-                                           latents: torch.Tensor,
-                                           indices_to_alter: List[int],
-                                           loss: torch.Tensor,
-                                           threshold: float,
-                                           text_embeddings: torch.Tensor,
-                                           text_input,
-                                           attention_store: AttentionStore,
-                                           step_size: float,
-                                           t: int,
-                                           attention_res: int = 16,
-                                           smooth_attentions: bool = True,
-                                           sigma: float = 0.5,
-                                           kernel_size: int = 3,
-                                           max_refinement_steps: int = 20,
-                                           normalize_eot: bool = False):
-        """
-        Performs the iterative latent refinement introduced in the paper. Here, we continuously update the latent
-        code according to our loss objective until the given threshold is reached for all tokens.
-        """
-        iteration = 0
-        target_loss = max(0, 1. - threshold)
-        while loss > target_loss:
-            iteration += 1
-
-            latents = latents.clone().detach().requires_grad_(True)
-            noise_pred_text = self.unet(latents, t, encoder_hidden_states=text_embeddings[1].unsqueeze(0)).sample
-            self.unet.zero_grad()
-
-            # Get max activation value for each subject token
-            max_attention_per_index = self._aggregate_and_get_max_attention_per_token(
-                attention_store=attention_store,
-                indices_to_alter=indices_to_alter,
-                attention_res=attention_res,
-                smooth_attentions=smooth_attentions,
-                sigma=sigma,
-                kernel_size=kernel_size,
-                normalize_eot=normalize_eot
-                )
-
-            loss, losses = self._compute_loss(max_attention_per_index, return_losses=True)
-
-            if loss != 0:
-                latents = self._update_latent(latents, loss, step_size)
-
-            with torch.no_grad():
-                noise_pred_uncond = self.unet(latents, t, encoder_hidden_states=text_embeddings[0].unsqueeze(0)).sample
-                noise_pred_text = self.unet(latents, t, encoder_hidden_states=text_embeddings[1].unsqueeze(0)).sample
-
-            try:
-                low_token = np.argmax([l.item() if type(l) != int else l for l in losses])
-            except Exception as e:
-                print(e)  # catch edge case :)
-                low_token = np.argmax(losses)
-
-            low_word = self.tokenizer.decode(text_input.input_ids[0][indices_to_alter[low_token]])
-            print(f'\t Try {iteration}. {low_word} has a max attention of {max_attention_per_index[low_token]}')
-
-            if iteration >= max_refinement_steps:
-                print(f'\t Exceeded max number of iterations ({max_refinement_steps})! '
-                      f'Finished with a max attention of {max_attention_per_index[low_token]}')
-                break
-
-        # Run one more time but don't compute gradients and update the latents.
-        # We just need to compute the new loss - the grad update will occur below
-        latents = latents.clone().detach().requires_grad_(True)
-        noise_pred_text = self.unet(latents, t, encoder_hidden_states=text_embeddings[1].unsqueeze(0)).sample
-        self.unet.zero_grad()
-
-        # Get max activation value for each subject token
-        max_attention_per_index = self._aggregate_and_get_max_attention_per_token(
-            attention_store=attention_store,
-            indices_to_alter=indices_to_alter,
-            attention_res=attention_res,
-            smooth_attentions=smooth_attentions,
-            sigma=sigma,
-            kernel_size=kernel_size,
-            normalize_eot=normalize_eot)
-        loss, losses = self._compute_loss(max_attention_per_index, return_losses=True)
-        print(f"\t Finished with loss of: {loss}")
-        return loss, latents, max_attention_per_index
 
     @torch.no_grad()
     def __call__(
@@ -516,19 +379,19 @@ class AttendAndExcitePipeline(StableDiffusionPipeline):
                                                 encoder_hidden_states=prompt_embeds[1].unsqueeze(0), cross_attention_kwargs=cross_attention_kwargs).sample
                     self.unet.zero_grad()
 
-                    # Get max activation value for each subject token
-                    max_attention_per_index = self._aggregate_and_get_max_attention_per_token(
-                        attention_store=attention_store,
-                        indices_to_alter=indices_to_alter,
-                        attention_res=attention_res,
-                        smooth_attentions=smooth_attentions,
-                        sigma=sigma,
-                        kernel_size=kernel_size,
-                        normalize_eot=sd_2_1)
+                    # # Get max activation value for each subject token
+                    # max_attention_per_index = self._aggregate_and_get_max_attention_per_token(
+                    #     attention_store=attention_store,
+                    #     indices_to_alter=indices_to_alter,
+                    #     attention_res=attention_res,
+                    #     smooth_attentions=smooth_attentions,
+                    #     sigma=sigma,
+                    #     kernel_size=kernel_size,
+                    #     normalize_eot=sd_2_1)
 
                     if not run_standard_sd:
 
-                        loss = self._compute_loss(max_attention_per_index=max_attention_per_index)
+                        # loss = self._compute_loss(max_attention_per_index=max_attention_per_index)
 
                         # If this is an iterative refinement step, verify we have reached the desired threshold for all
                         if i in thresholds.keys() and loss > 1. - thresholds[i]:
